f6c69e0e53d4cde7faf88ade3e3ee16a1f47f6b8
Carmelina Jaramillo <Horizonsbmth@gmail.com>
2024-02-15 23:55:13 -0800

    Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//Identify: Carmelina Jaramillo, 2/15/2024
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //Assign the Variables
+        String day = "Friday";
+        int date = 15;
+        String month = "February";
+        int year = 2024;
+
+        //Print variables as the Sample Output
+        System.out.println("Day = " + day);
+        System.out.println("Date = " + date);
+        System.out.println("Month = " + month);
+        System.out.println("Year = " + year);
+
+        //Print variables in a Single Line
+        System.out.println("Day = " + day + ", Date = " + date + ", Month = " + month + ", Year = " + year);
+    }
+}
